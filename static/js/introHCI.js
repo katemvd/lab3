8abf8071f5d4f7bef15afc80dcd0df77bc6cdaeb
Kate Van Donge <katemvd@gmail.com>
2017-01-26 10:31:13 -0800

    hello

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,32 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+		$('.jumbotron h1').text("I changed it!");
+        $("#testjs").text("Please wait...");
+        $(".jumbotron p").addClass("active");
 
+	});
+    $(".project").click(function(e){
+        projectClick(e);
+    });
+}
 	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    $(this).css("background-color", "#7fff00");
+    
+    var containingProject = $(e.target).closest(".project");
+    //containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+    //var containingProject = $(e.target).closest(".project");
+    var description = $(containingProject).find(".project-description");
+   
+   if (description.length == 0) {
+       containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       $(description).toggle();
+    }
+
+                                                       
 }
